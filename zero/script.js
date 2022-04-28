24b1d17a5373c5299175bfe0d5cac00b88fcc3dd
ZeroFuruya <99241183+ZeroFuruya@users.noreply.github.com>
2022-04-28 23:25:24 +0800

    Updated

--- zero/script.js
+++ zero/script.js
@@ -12,8 +12,27 @@ profileButton.addEventListener('click', () => {
     profileLinks.classList.toggle("active-menu");
 });
 
-window.onclick = function(event) {
-    if (event.target == profileLinks) {
-        profileLinks.style.display = "none";
-    }
-}
+// PARALLAX
+
+var twoBG = document.getElementById("two");
+var threeBG = document.getElementById("three");
+var fourBG = document.getElementById("four");
+var fiveBG = document.getElementById("five");
+var sixBG = document.getElementById("six");
+var sevenBG = document.getElementById("seven");
+var eightBG = document.getElementById("eight");
+const introDuction = document.querySelector(".intro");
+
+window.addEventListener('scroll', () => {
+    var offsetY = window.pageYOffset;
+    profileButton.style.transform = "rotate(" + offsetY * 0.25 + "deg)";
+    introDuction.style.transform = "translateX(" + offsetY * 1 + "px)";
+    twoBG.style.top = offsetY * 0.4 + "px";
+    threeBG.style.top = offsetY * 0.5 + "px";
+    fourBG.style.top = offsetY * 0.6 + "px";
+    fiveBG.style.top = offsetY * 0.7 + "px";
+    sixBG.style.top = offsetY * 0.8 + "px";
+    sevenBG.style.top = offsetY * 0.9 + "px";
+    eightBG.style.top = offsetY * 1 + "px";
+    console.log(offsetY)
+});
