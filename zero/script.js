8f82242f3fc34b94160ce347b44528f163f5b4f9
ZeroFuruya <99241183+ZeroFuruya@users.noreply.github.com>
2022-04-29 23:50:27 +0800

    updated

--- zero/script.js
+++ zero/script.js
@@ -21,18 +21,38 @@ var fiveBG = document.getElementById("five");
 var sixBG = document.getElementById("six");
 var sevenBG = document.getElementById("seven");
 var eightBG = document.getElementById("eight");
-const introDuction = document.querySelector(".intro");
 
 window.addEventListener('scroll', () => {
     var offsetY = window.pageYOffset;
     profileButton.style.transform = "rotate(" + offsetY * 0.25 + "deg)";
-    introDuction.style.transform = "translateX(" + offsetY * 1 + "px)";
-    twoBG.style.top = offsetY * 0.4 + "px";
-    threeBG.style.top = offsetY * 0.5 + "px";
-    fourBG.style.top = offsetY * 0.6 + "px";
-    fiveBG.style.top = offsetY * 0.7 + "px";
-    sixBG.style.top = offsetY * 0.8 + "px";
-    sevenBG.style.top = offsetY * 0.9 + "px";
-    eightBG.style.top = offsetY * 1 + "px";
-    console.log(offsetY)
+    if (offsetY <= 700) {
+        delayElements();
+        twoBG.style.top = offsetY * 0.4 + "px";
+        threeBG.style.top = offsetY * 0.5 + "px";
+        fourBG.style.top = offsetY * 0.6 + "px";
+        fiveBG.style.top = offsetY * 0.7 + "px";
+        sevenBG.style.top = offsetY * 0.9 + "px";
+        eightBG.style.top = offsetY * 1 + "px";
+
+        var rect = eightBG.getBoundingClientRect(); //gets x and y position of an element
+
+    } else {
+        stickElements();
+    }
+
+
+    function stickElements() {
+        var allBG = document.querySelectorAll(".bg");
+        allBG.forEach(el => {
+            el.style.top = rect;
+        });
+    }
+
+    function delayElements() {
+        var allBG = document.querySelectorAll(".bg");
+        allBG.forEach(el => {
+            el.style.transition = "500ms";
+        })
+    }
+
 });
